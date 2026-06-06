20e2e7e81611c4ae50f265c39ca8b00e3e4ff0bd
SolveSaint <solvesaint5@gmail.com>
2026-06-06 19:31:51 -0400

    Compact article title rendering

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -4,7 +4,7 @@ import { classNames } from "../util/lang"
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
   if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+    return <h1 class={classNames(displayClass, "article-title compact-article-title")}>{title}</h1>
   } else {
     return null
   }
@@ -13,6 +13,31 @@ const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzCompone
 ArticleTitle.css = `
 .article-title {
   margin: 2rem 0 0 0;
+}
+
+.compact-article-title {
+  display: inline-block !important;
+  width: fit-content !important;
+  max-width: 32rem !important;
+  margin-top: 0.18rem !important;
+  margin-bottom: 1.05rem !important;
+  padding-top: 0.06em !important;
+  padding-right: 0.24em !important;
+  padding-bottom: 0.08em !important;
+  padding-left: 0.96rem !important;
+  line-height: 1.02 !important;
+  font-size: 2rem !important;
+}
+
+.compact-article-title::before {
+  inset: -0.035em -0.12em !important;
+  border-radius: 8px !important;
+}
+
+.compact-article-title::after {
+  left: 0.22rem !important;
+  top: 0.26em !important;
+  font-size: 0.92em !important;
 }
 `
 
