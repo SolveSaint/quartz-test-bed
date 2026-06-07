7a5cde0fd3d5b4694895b4308e5c571c684377b1
SolveSaint <182136470+SolveSaint@users.noreply.github.com>
2026-06-07 18:33:11 -0400

    Force remove fog from rendered background stack

--- quartz/components/FogShapeStyles.tsx
+++ quartz/components/FogShapeStyles.tsx
@@ -1,30 +1,23 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 
-const FogShapeStyles: QuartzComponent = () => null
-
-FogShapeStyles.css = `
-body::before {
-  background-image:
-    url("./static/trees-row-front.svg"),
-    url("./static/trees-row-mid.svg"),
-    url("./static/trees-row-far.svg"),
-    url("./static/stars-twinkle.svg") !important;
-  background-repeat: repeat-x, repeat-x, repeat-x, repeat !important;
-  background-size:
-    1600px 760px,
-    1800px 700px,
-    1900px 560px,
-    1400px 900px !important;
-  background-position:
-    0 calc(100% + 24vh),
-    0 calc(100% + 2vh),
-    0 100%,
-    0 0 !important;
+const FogShapeStyles: QuartzComponent = () => {
+  return (
+    <style>{`
+      html body::before {
+        background-image:
+          url("/quartz-test-bed/static/trees-row-front.svg"),
+          url("/quartz-test-bed/static/trees-row-mid.svg"),
+          url("/quartz-test-bed/static/trees-row-far.svg"),
+          url("/quartz-test-bed/static/stars-twinkle.svg") !important;
+        background-repeat: repeat-x, repeat-x, repeat-x, repeat !important;
+        background-size: 1600px 760px, 1800px 700px, 1900px 560px, 1400px 900px !important;
+        background-position: 0 calc(100% + 24vh), 0 calc(100% + 2vh), 0 100%, 0 0 !important;
+      }
+      html body::after {
+        background: transparent !important;
+      }
+    `}</style>
+  )
 }
-
-body::after {
-  background: transparent !important;
-}
-`
 
 export default (() => FogShapeStyles) satisfies QuartzComponentConstructor
