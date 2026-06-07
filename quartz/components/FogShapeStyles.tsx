e5ef650617a68e708aed5d3e23e646b3dc36019b
SolveSaint <182136470+SolveSaint@users.noreply.github.com>
2026-06-07 17:16:05 -0400

    Load fog reset stylesheet

--- quartz/components/FogShapeStyles.tsx
+++ quartz/components/FogShapeStyles.tsx
@@ -1,47 +1,7 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 
 const FogShapeStyles: QuartzComponent = () => {
-  return (
-    <style>{`
-      body::before {
-        background-image:
-          url("/quartz-test-bed/static/trees-row-front.svg"),
-          url("/quartz-test-bed/static/fog-wisps-near.svg"),
-          radial-gradient(ellipse at 78% 70%, rgba(176,140,230,0.18), transparent 22rem),
-          url("/quartz-test-bed/static/trees-row-mid.svg"),
-          url("/quartz-test-bed/static/fog-wisps-mid.svg"),
-          radial-gradient(ellipse at 70% 60%, rgba(105,220,155,0.18), transparent 28rem),
-          url("/quartz-test-bed/static/trees-row-far.svg"),
-          url("/quartz-test-bed/static/fog-wisps-far.svg"),
-          radial-gradient(ellipse at 48% 46%, rgba(135,190,235,0.20), transparent 34rem),
-          url("/quartz-test-bed/static/stars-twinkle.svg") !important;
-        background-repeat: repeat-x, repeat-x, no-repeat, repeat-x, repeat-x, no-repeat, repeat-x, repeat-x, no-repeat, repeat !important;
-        background-size:
-          1600px 760px,
-          1700px 90vh,
-          720px 220px,
-          1800px 700px,
-          1900px 80vh,
-          980px 280px,
-          1900px 560px,
-          2200px 75vh,
-          1200px 360px,
-          1400px 900px !important;
-        background-position:
-          0 calc(100% + 24vh),
-          -12vw 10vh,
-          70vw 70vh,
-          0 calc(100% + 2vh),
-          8vw 14vh,
-          62vw 60vh,
-          0 100%,
-          -18vw 8vh,
-          20vw 46vh,
-          0 0 !important;
-        animation-name: qtbLayeredParallax;
-      }
-    `}</style>
-  )
+  return <link rel="stylesheet" href="/quartz-test-bed/static/fog-reset.css" />
 }
 
 export default (() => FogShapeStyles) satisfies QuartzComponentConstructor
