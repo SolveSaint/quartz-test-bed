b1962ad490c5f69154a2bff6dd512a9c5a22d397
SolveSaint <182136470+SolveSaint@users.noreply.github.com>
2026-06-07 18:28:43 -0400

    Remove fog and align tree background layers

--- quartz/components/FogShapeStyles.tsx
+++ quartz/components/FogShapeStyles.tsx
@@ -9,6 +9,21 @@ body::before {
     url("./static/trees-row-mid.svg"),
     url("./static/trees-row-far.svg"),
     url("./static/stars-twinkle.svg") !important;
+  background-repeat: repeat-x, repeat-x, repeat-x, repeat !important;
+  background-size:
+    1600px 760px,
+    1800px 700px,
+    1900px 560px,
+    1400px 900px !important;
+  background-position:
+    0 calc(100% + 24vh),
+    0 calc(100% + 2vh),
+    0 100%,
+    0 0 !important;
+}
+
+body::after {
+  background: transparent !important;
 }
 `
 
