860200ef4df8fa0817d8fbf192438af639088b41
SolveSaint <182136470+SolveSaint@users.noreply.github.com>
2026-06-07 13:28:31 -0400

    Set Quartz base URL

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,9 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
-const config = await loadQuartzConfig()
+const config = await loadQuartzConfig({
+  pageTitle: "Quartz Test Bed",
+  baseUrl: "solvesaint.github.io/quartz-test-bed",
+})
+
 export default config
 export const layout = await loadQuartzLayout()
