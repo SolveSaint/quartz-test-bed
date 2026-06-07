0bc787c0a8b9f0e5b57e5fbedf98b4e3e87dc04e
SolveSaint <182136470+SolveSaint@users.noreply.github.com>
2026-06-07 16:55:48 -0400

    Mount fog shape styles

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,10 +1,11 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import FogShapeStyles from "./quartz/components/FogShapeStyles"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.HeaderImage()],
+  header: [FogShapeStyles(), Component.HeaderImage()],
   afterBody: [Component.Postscript()],
   footer: Component.Footer({
     links: {
